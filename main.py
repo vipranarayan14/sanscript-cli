55c1166edbc5544b746158617b53851ec5d76bb0
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2021-05-09 23:50:14 +0530

    refactor(main): move utility funcs to 'utils.py'
    Why?
    - for decluttering code
    - for maintainability
    - for better organisation

--- main.py
+++ main.py
@@ -1,48 +1,16 @@
-import sys
-from os import path
 from typing import Optional
 
 import typer
 from indic_transliteration.sanscript import transliterate, SCHEMES, SchemeMap
 
 import help_text
+from utils import get_input_data, write_output
 
 app = typer.Typer()
 
 scheme_names = list(SCHEMES.keys())
 
 scheme_help = "Choose from: {}.".format(", ".join(scheme_names))
-
-
-def get_input_data(input_file: typer.FileText, input_string: str) -> str:
-    if input_file is not None:
-        if input_string is not None:
-            typer.echo(
-                "Warning: The input string is ignored since input file is specified.",
-                err=True,
-            )
-        return input_file.read()
-
-    if input_string is not None:
-        return input_string
-
-    typer.echo(
-        "Error: Either a string or a file is required as input. See help with '--help'.",
-        err=True,
-    )
-    raise typer.Exit(code=1)
-
-
-def write_output(output_file: typer.FileTextWrite, output_data: str):
-    if output_file is None:
-        return typer.echo(output_data)
-
-    output_file.write(output_data)
-
-    if output_file is sys.stdout:
-        return
-
-    typer.echo(f"Output written to: {path.realpath(output_file.name)}")
 
 
 def check_scheme(scheme_name: str):
---
+++ utils.py
@@ -0,0 +1,35 @@
+import sys
+from os import path
+
+import typer
+
+
+def get_input_data(input_file: typer.FileText, input_string: str) -> str:
+    if input_file is not None:
+        if input_string is not None:
+            typer.echo(
+                "Warning: The input string is ignored since input file is specified.",
+                err=True,
+            )
+        return input_file.read()
+
+    if input_string is not None:
+        return input_string
+
+    typer.echo(
+        "Error: Either a string or a file is required as input. See help with '--help'.",
+        err=True,
+    )
+    raise typer.Exit(code=1)
+
+
+def write_output(output_file: typer.FileTextWrite, output_data: str):
+    if output_file is None:
+        return typer.echo(output_data)
+
+    output_file.write(output_data)
+
+    if output_file is sys.stdout:
+        return
+
+    typer.echo(f"Output written to: {path.realpath(output_file.name)}")
