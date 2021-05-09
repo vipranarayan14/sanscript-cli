c69a5439fd7ae2c4b4288879fd8819465a457b01
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2021-05-09 20:43:40 +0530

    feat(main): show output file path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from os import path
 
 import typer
 from indic_transliteration.sanscript import transliterate, SCHEMES, SchemeMap
@@ -29,6 +30,7 @@ def get_input_data(input_file: typer.FileText, input_string: str) -> str:
 def write_output(output_file: typer.FileTextWrite, output_data: str):
     if output_file is not None:
         output_file.write(output_data)
+        typer.echo(f"Output written to: {path.realpath(output_file.name)}")
     else:
         typer.echo(output_data)
 
