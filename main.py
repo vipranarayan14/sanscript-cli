78cd22f88634d8b8f4cc8ca62e20516894e4823c
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2021-05-09 23:29:07 +0530

    refactor(main): flatten nested `if` blocks
    Why?
    - to reduce complexity
    - to improve readability
    - to reduce branching

--- main.py
+++ main.py
@@ -22,30 +22,35 @@ def get_input_data(input_file: typer.FileText, input_string: str) -> str:
                 err=True,
             )
         return input_file.read()
-    elif input_string is not None:
+
+    if input_string is not None:
         return input_string
-    else:
-        typer.echo(
-            "Error: Either a string or a file is required as input. See help with '--help'.",
-            err=True,
-        )
-        raise typer.Exit(code=1)
+
+    typer.echo(
+        "Error: Either a string or a file is required as input. See help with '--help'.",
+        err=True,
+    )
+    raise typer.Exit(code=1)
 
 
 def write_output(output_file: typer.FileTextWrite, output_data: str):
-    if output_file is not None:
-        output_file.write(output_data)
-        if output_file is not sys.stdout:
-            typer.echo(f"Output written to: {path.realpath(output_file.name)}")
-    else:
-        typer.echo(output_data)
+    if output_file is None:
+        return typer.echo(output_data)
+
+    output_file.write(output_data)
+
+    if output_file is sys.stdout:
+        return
+
+    typer.echo(f"Output written to: {path.realpath(output_file.name)}")
 
 
 def check_scheme(scheme_name: str):
-    if scheme_name not in scheme_names:
-        error_msg = f"Invalid scheme name. {scheme_help}"
-        raise typer.BadParameter(error_msg)
-    return scheme_name
+    if scheme_name in scheme_names:
+        return scheme_name
+
+    error_msg = f"Invalid scheme name. {scheme_help}"
+    raise typer.BadParameter(error_msg)
 
 
 typer_opt_from_scheme = typer.Option(
